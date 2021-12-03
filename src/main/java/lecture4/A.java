bff51d385a95f959d6347c296a4f692af2cdfeda
mariiakarpenko <mariiaktest@gmail.com>
2021-12-03 15:19:08 +0200

    lecture4

---
+++ src/main/java/lecture4/A.java
@@ -0,0 +1,12 @@
+package lecture4;
+
+public class A {
+    int i, j;
+    A(int a, int b){
+       a = i;
+       b = j;
+    }
+    void show(){
+        System.out.println("i и j: " + i + " " + j);
+    }
+}
